780312a17479252622c33b1c678ce231232ff56d
OksanaBakhaieva <obahaeva@gmail.com>
2025-02-01 02:30:56 +0000

    updated styles

--- src/Components/Header/HeaderTop.jsx
+++ src/Components/Header/HeaderTop.jsx
@@ -3,16 +3,17 @@ import css from './HeaderTop.module.css';
 
 function HeaderTop() {
     return (
-            <div className={css.header_top_container}>
-                <LanguageSwitcher /> 
-                <address>
-                    <a className={css.phone} href="tel:447748xxxxxx">
-                        <svg  className={css.phone_icon} width="24" height="24">
-                            <use href="../../img/icons.svg#icon-phone"></use>
-                        </svg>
-                        + (44) 77 48 300 xxx</a>
-                </address>    
-            </div>
+        <div className={css.header_top_container}>
+             <svg  className={css.earth} width="24" height="24">
+                <use href="../../img/icons.svg#icon-earth"></use>
+            </svg>
+            <LanguageSwitcher /> 
+            <a className={css.phone} href="tel:447748300645">
+                <svg  className={css.phone_icon} width="24" height="24">
+                    <use href="../../img/icons.svg#icon-phone"></use>
+                </svg>
+            </a>
+        </div>
     )
 }
 
--- src/Components/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/Components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -8,11 +8,7 @@ function LanguageSwitcher() {
     
     return (
         <div className={css.languageSwitcher}>
-            <svg  className={css.earth} width="24" height="24">
-                <use href="../../img/icons.svg#icon-earth"></use>
-            </svg>
-        
-            <p className={css.text}>Availiable  in  
+           <p className={css.text}>Availiable  in  
                 <button className={css.span} onClick={() => setLanguage("en")}>English  /</button>
                 <button className={css.span} onClick={() => setLanguage("uk")}>Ukrainain  /</button>
                 <button className={css.span} onClick={() => setLanguage("ru")}>Russian</button>
