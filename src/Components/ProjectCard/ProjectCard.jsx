84cc7eed2eec1d9ad89f975a5783235a74b140b3
OksanaBakhaieva <obahaeva@gmail.com>
2025-02-01 01:45:17 +0000

    fixed images at the projects section

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -3,9 +3,8 @@ import css from './ProjectCard.module.css';
 function ProjectCard({name, img, desc, url }) {
     return (
         <>
-            <img src={img} alt="Web Page of Photography School" className={css.item_img} width="230" />
             <a href={url} className={css.link}>
-                
+                <img src={img} alt="Web Page of Photography School" className={css.item_img} width="230" />
                 <h3 className={css.item_title}>{name}</h3>
             </a>
             <p className={css.item_description}>{desc}</p>
